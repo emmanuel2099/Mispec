6a3de2ac7c6c9dc01f3884e7a5cc0042c505c5a9
emmapk <ahamefulammadu@gmail.com>
2026-05-06 23:50:38 -0700

    Switch OTP email to Django SMTP (Gmail)

--- users/twilio_utils.py
+++ users/twilio_utils.py
@@ -1,42 +1,24 @@
-import requests
+from django.core.mail import send_mail
 from django.conf import settings
 
 
 def send_otp(email, otp):
-    html_content = f"""<html><body>
-    <p>Hi,<br>We're excited to have you get started.</p>
-    <br><p>Please verify your account using the code below:<br>
-    <strong style="font-size:24px;">{otp}</strong></p><br>
-    <p>Note: This code expires in 5 minutes.
-    If this wasn't done by you, contact <a href="mailto:Info@mispec.co.uk">Info@mispec.co.uk</a></p><br>
-    <p>Thanks for choosing Mispec.</p>
-    </body></html>"""
+    html_content = f"""
+    Hi,<br>We're excited to have you get started.<br><br>
+    Please verify your account using the code below:<br>
+    <strong style="font-size:24px;">{otp}</strong><br><br>
+    Note: This code expires in 5 minutes.
+    If this wasn't done by you, contact <a href="mailto:Info@mispec.co.uk">Info@mispec.co.uk</a><br><br>
+    Thanks for choosing Mispec.
+    """
 
-    payload = {
-        "recipients": [{"address": {"email": email}}],
-        "body": {
-            "type": "html",
-            "html": {"data": html_content},
-        },
-        "from": {
-            "address": {
-                "email": settings.DEFAULT_FROM_EMAIL,
-                "name": "Mispec",
-            }
-        },
-        "subject": "Your OTP Code",
-    }
-
-    response = requests.post(
-        "https://api.bird.com/workspaces/me/channels/me/messages",
-        json=payload,
-        headers={
-            "Authorization": f"AccessKey {settings.BIRD_EMAIL_API_KEY}",
-            "Content-Type": "application/json",
-        },
+    send_mail(
+        subject="Your OTP Code",
+        message=f"Your OTP code is: {otp}. It expires in 5 minutes.",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[email],
+        html_message=html_content,
+        fail_silently=False,
     )
 
-    if response.status_code not in (200, 201):
-        raise Exception(f"Bird error {response.status_code}: {response.text}")
-
-    print(f"OTP email sent to {email} via Bird")
+    print(f"OTP email sent to {email} via Django SMTP")
